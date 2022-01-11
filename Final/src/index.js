cba8387d5c2b7f28c2fc136b14a5692343517b32
Dreammary0 <86566838+Dreammary0@users.noreply.github.com>
2022-01-12 00:34:08 +1000

    Add files via upload

---
+++ Final/src/index.js
@@ -0,0 +1,9 @@
+const axios = require('axios').default;
+axios.all([
+    axios.get('https://api.github.com/users/Dreammary0')
+])
+    .then(response => {
+        document.getElementById('text').innerText=('Login: '+ response[0].data.login+'\nDate created: '+ response[0].data.created_at);
+    });
+
+
